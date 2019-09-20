92490d473efd48a0f2d2f4963a127102d2f9a55e
Krzysztof Branecki <krzysztof@branecki.net>
2019-09-20 11:14:19 +0200

    Add Middleware to /store

--- index.js
+++ index.js
@@ -11,11 +11,25 @@ fs.readFile("./data.json", "utf8", function(err, data) {
   stringifyFile = data;
 });
 
+app.use("/store", function(req, res, next) {
+  console.log("Jestem pośrednikiem przy żądaniu do /store");
+  next();
+});
+
+app.use(function(req, res, next) {
+  console.log("Hej, jestem pośrednikiem między żądaniem a odpowiedzią!");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(express.static("assets"));
 
 app.get("/", function(req, res) {
   res.sendFile("/index.html");
+});
+
+app.get("/store", function(req, res) {
+  res.send("To jest sklep");
 });
 
 app.get("/userform", function(req, res) {
